b7811d4cbdbc4bd6b29f1d6432f9efbded04186e
Nils Fohlin <nils.fohlin@chromaway.com>
2022-03-29 16:52:51 -0500

    collection struct

--- contracts/Corite_ERC1155.sol
+++ contracts/Corite_ERC1155.sol
@@ -25,8 +25,16 @@ contract Corite_ERC1155 is ERC1155Supply, AccessControl{
         bool closed;
     }
 
-    mapping (uint => uint) public nftCount;
+    struct Collection {
+        address owner;
+
+        uint totalSupply;
+        uint minted;
+    }
+
     mapping (address => uint[]) public ownedCollections;
+    mapping (uint => Collection) public collectionInfo;
+
     mapping (address => uint[]) public ownedCampaigns;
     mapping (uint => Campaign) public campaignInfo;
 
@@ -45,31 +53,31 @@ contract Corite_ERC1155 is ERC1155Supply, AccessControl{
         _;
     }
 
-    function createCollection(address _owner) external isCREATE_CLOSE_HANDLER() returns(uint collection) {
+    function createCollection(address _owner, uint _totalSupply) external isCREATE_CLOSE_HANDLER() returns(uint collection) {
+        require(_totalSupply > 0, "Cap can not be 0");
         nftCollection++;
-        ownedCollections[_owner].push(nftCollection);
         collection = getFullCollectionId(nftCollection);
-        nftCount[nftCollection] = collection;
+        ownedCollections[_owner].push(collection);
+        collectionInfo[collection] = Collection({owner: _owner, totalSupply: _totalSupply, minted: collection});
     }
 
     function mintCollectionBatch(uint _collection, uint _amount, address _to) external isMINTER_BURNER_HANDLER() {
-        require(_collection > 0 && _collection < nftCollection , "Collection does not exist");
-        uint fci = getFullCollectionId(_collection);
+        require(collectionInfo[_collection].minted + _amount <= collectionInfo[_collection].totalSupply , "Amount exceeds supply");
         for (uint i = 0; i < _amount; i++) {
-            nftCount[_collection]++;
-            _mint(_to, fci + nftCount[_collection], 1, "");            
+            collectionInfo[_collection].minted++;
+            _mint(_to, collectionInfo[_collection].minted, 1, "");            
         }
     }
 
     function mintCollectionSingle(uint _collection, address _to) external isMINTER_BURNER_HANDLER() {
-        require(_collection > 0 && _collection <= nftCollection , "Collection does not exist");
-        nftCount[_collection]++;
-        _mint(_to, getFullCollectionId(_collection) + nftCount[_collection], 1, "");            
+        collectionInfo[_collection].minted++;
+        require(collectionInfo[_collection].minted <= collectionInfo[_collection].totalSupply , "Minted exceeds supply");
+        _mint(_to, collectionInfo[_collection].minted, 1, "");            
     }
 
     function createCampaign(address _owner, uint _valuationUSD, uint _totalSupply, uint _maxFundingAllowed) external isCREATE_CLOSE_HANDLER() returns(uint){
         require(_totalSupply > 0 && _maxFundingAllowed > 0, "Cannot creat campaign with 0 shares or 0 allowed to sell");
-        require(_totalSupply >= _maxFundingAllowed , "supply much be greater or equal to max allowed");
+        require(_totalSupply >= _maxFundingAllowed , "Supply much be greater or equal to max allowed");
 
         campaignCount++;
         ownedCampaigns[_owner].push(campaignCount);
@@ -106,7 +114,7 @@ contract Corite_ERC1155 is ERC1155Supply, AccessControl{
     }
 
     function getFullCollectionId(uint _collection) public pure returns (uint){
-        return (2 * (10 ** 68)) + (_collection * (10 ** 48));
+        return (2 * (10 ** 68)) + (_collection * (10 ** 60));
     }
 
     function uri(uint _tokenId) override public view returns (string memory) {
