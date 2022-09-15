e3c6ec20bbe52c8d95ff176d8f1c0d703f6177cd
Koray Kaya <koray.kaya@chromaway.com>
2022-09-15 12:52:38 +0200

    added preliminary claim function

--- contracts/COStake.sol
+++ contracts/COStake.sol
@@ -3,22 +3,32 @@
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/access/AccessControl.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 import "../interfaces/ITwoWeeksNotice.sol";
 
 contract COStake is AccessControl {
     ITwoWeeksNotice public twoWeeksNotice;
+    IERC20 public token;
+    mapping(address => uint) claimedCO;
 
-    constructor(ITwoWeeksNotice _twoWeeksNotice) {
+    constructor(ITwoWeeksNotice _twoWeeksNotice, IERC20 _token) {
         twoWeeksNotice = _twoWeeksNotice;
+        token = _token;
     }
 
-    function estimateReward() public view returns (uint) {
+    function estimateYield() public view returns (uint) {
         (uint128 accumulated, uint128 accumulatedStrict) = twoWeeksNotice
             .estimateAccumulated(msg.sender);
 
-        uint reward = accumulatedStrict / 1460;
-        return reward;
+        uint yield = accumulatedStrict / 1460 - claimedCO[msg.sender];
+        return yield;
+    }
+
+    function claimYield() external {
+        uint claimableYield = estimateYield();
+        require(claimableYield > 0, "No claimable yield");
+        claimedCO[msg.sender] += claimableYield;
+        token.transfer(msg.sender, claimableYield);
     }
 }
