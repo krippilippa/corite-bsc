218efe2bc076bddf838123a49f43fd2fa25e5d3b
Koray Kaya <koray.kaya@chromaway.com>
2022-09-19 11:57:58 +0200

    cleanup

--- contracts/staking/COStake.sol
+++ contracts/staking/COStake.sol
@@ -2,7 +2,6 @@
 
 pragma solidity ^0.8.4;
 
-import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
@@ -20,7 +19,7 @@ contract COStake is AccessControl {
     }
     // denotes a change in yield
     struct YieldPoint {
-        uint yieldRate; // New yield rate
+        uint yieldRate; // New yield rate in the format of (1/percentage)*365
         uint timestamp; // timestamp of change
     }
 
