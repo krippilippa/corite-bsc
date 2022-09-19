f79e381e00472edd0dd4718167f3f06668d6b351
Koray Kaya <koray.kaya@chromaway.com>
2022-09-19 11:17:54 +0200

    added custom faucet func

--- contracts/helpers/TestCO.sol
+++ contracts/helpers/TestCO.sol
@@ -10,6 +10,10 @@ contract TestCO is ERC20 {
         _mint(msg.sender, 1000000000);
     }
 
+    function faucet2(uint amount) external {
+        _mint(msg.sender, amount);
+    }
+
     function decimals() public pure override returns (uint8) {
         return 6;
     }
