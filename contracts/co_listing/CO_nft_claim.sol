ebadbe167ceac8d5ca91230f8a235aaecc94b8b3
Nils Fohlin <nils.fohlin@chromaway.com>
2022-09-28 11:03:40 +0200

    claim co

---
+++ contracts/co_listing/CO_nft_claim.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/interfaces/IERC20.sol";
+
+contract CO_claim is AccessControl , Pausable{
+
+    IERC20 public CO_token;
+    address private serverPubKey;
+
+    mapping (address => uint) public internalNonce;
+ 
+    constructor(IERC20 _CO_token, address _serverPubKey, address _default_admin_role) {
+        CO_token = _CO_token;
+        serverPubKey = _serverPubKey;
+        _setupRole(DEFAULT_ADMIN_ROLE, _default_admin_role);
+    }
+
+    function claimCO(
+        uint _amount,
+        uint8 _v,
+        bytes32 _r,
+        bytes32 _s
+    ) external whenNotPaused{
+        bytes memory message = abi.encode(msg.sender, _amount, internalNonce[msg.sender]);
+        require(ecrecover(keccak256(abi.encodePacked( message)), _v, _r, _s) == serverPubKey, "Signature invalid");
+        internalNonce[msg.sender]++;
+        CO_token.transfer(msg.sender, _amount);
+    }
+
+    function changeServerKey(address _sK) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        serverPubKey = _sK;
+    }
+
+
+    function pauseHandler() public onlyRole(DEFAULT_ADMIN_ROLE) {
+        _pause();
+    }
+
+    function unpauseHandler() public onlyRole(DEFAULT_ADMIN_ROLE) {
+        _unpause();
+    }
+}
