c00b800a616ae24550dbcbfac750f1c47c6792fe
Koray Kaya <koray.kaya@chromaway.com>
2022-10-07 10:10:34 +0200

    deploy ready

--- scripts/deploy-OriginsNFTBurn.js
+++ scripts/deploy-OriginsNFTBurn.js
@@ -2,43 +2,18 @@ async function main() {
   // Contracts are deployed using the first signer/account by default
   const [owner, otherAccount] = await ethers.getSigners();
 
-  const TestCO = await ethers.getContractFactory("TestCO");
-  const testCO = await TestCO.deploy();
-
-  await testCO.deployed();
-
-  await testCO.faucet();
-  console.log("CO: " + testCO.address);
-
-  const Test721 = await ethers.getContractFactory("CoriteMNFT");
-  const test721 = await Test721.deploy(
-    ethers.constants.AddressZero,
-    owner.address
-  );
-
-  await test721.deployed();
-
-  console.log("721: " + test721.address);
-
   const OriginsNFTBurn = await ethers.getContractFactory("OriginsNFTBurn");
   const originsNFTBurn = await OriginsNFTBurn.deploy(
-    test721.address,
-    testCO.address,
-    owner.address,
+    "0xA00de4aAe3E1d8726d822CE4A5DA52be4c3FfB28",
+    "0x6ee5DA30876E1697cD4Fc217d2A8a44D5A53A77d",
+    "0x51E6a589dd3D829FBd720B2f8af68F881E2D4FC1",
     "0x51E6a589dd3D829FBd720B2f8af68F881E2D4FC1",
     "0x51E6a589dd3D829FBd720B2f8af68F881E2D4FC1"
   );
 
   await originsNFTBurn.deployed();
 
-  console.log("contract " + originsNFTBurn.address);
-
-  await test721.grantRole(
-    ethers.utils.keccak256(ethers.utils.toUtf8Bytes("BURNER")),
-    originsNFTBurn.address
-  );
-
-  await testCO.increaseAllowance(originsNFTBurn.address, 10000000);
+  console.log("Burn contract: " + originsNFTBurn.address);
 }
 
 main()
